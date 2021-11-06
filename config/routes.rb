7269b615598c5c771f5a4262e0c81b1cad0c9d47
ayacatt <k.saiko0224@gmail.com>
2021-11-06 06:05:48 +0000

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  
+  scope module: :user do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+  
+  end 
+  
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :users,skip: [:passwords,], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+end
